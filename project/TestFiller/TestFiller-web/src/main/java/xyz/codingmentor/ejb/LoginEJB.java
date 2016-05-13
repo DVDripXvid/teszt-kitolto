bd2a828eee29e394d72d6f8b21888e4b6c797953
Rotttenhoffer Olivér <dvdrip.xvid.avi@gmail.com>
2016-05-13 14:38:24 +0200

    modify LoginEJB

--- project/TestFiller/TestFiller-web/src/main/java/xyz/codingmentor/ejb/LoginEJB.java
+++ project/TestFiller/TestFiller-web/src/main/java/xyz/codingmentor/ejb/LoginEJB.java
@@ -3,6 +3,7 @@ package xyz.codingmentor.ejb;
 import java.io.IOException;
 import java.io.Serializable;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import org.slf4j.Logger;
@@ -20,15 +21,16 @@ public class LoginEJB implements Serializable{
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginEJB.class);
 
     public void login(){
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();        
         String rolePath = "";
-        if(FacesContext.getCurrentInstance().getExternalContext().isUserInRole(RoleName.ADMIN)){
+        if(ec.isUserInRole(RoleName.ADMIN)){
             rolePath = "admin";
         }
-        if(FacesContext.getCurrentInstance().getExternalContext().isUserInRole(RoleName.TEACHER)){
+        if(ec.isUserInRole(RoleName.TEACHER)){
             rolePath = "teacher";
         }
         try {
-            FacesContext.getCurrentInstance().getExternalContext().redirect("http://localhost:1111/TestFiller-web-1.0-SNAPSHOT/faces/" + rolePath +"/index.xhtml");
+            ec.redirect(ec.getApplicationContextPath() + "/faces/" + rolePath + "/index.xhtml");
         } catch (IOException ex) {
             LOGGER.info(ex.getMessage());
         }
