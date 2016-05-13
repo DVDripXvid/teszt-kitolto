50c1751d584c979a5cac09afc5d0dd95f4c3f986
Rotttenhoffer Olivér <dvdrip.xvid.avi@gmail.com>
2016-05-13 13:47:30 +0200

    homepage

--- project/TestFiller/TestFiller-web/src/main/java/xyz/codingmentor/ejb/LoginEJB.java
+++ project/TestFiller/TestFiller-web/src/main/java/xyz/codingmentor/ejb/LoginEJB.java
@@ -1,10 +1,13 @@
-package hu.codingmentor.ejb;
+package xyz.codingmentor.ejb;
 
+import java.io.IOException;
 import java.io.Serializable;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import xyz.codingmentor.role.RoleName;
 
 /**
  *
@@ -16,8 +19,23 @@ public class LoginEJB implements Serializable{
     
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginEJB.class);
 
-    public String login(){
-        LOGGER.info("pressed");
-        return "admin";
+    public void login(){
+        String rolePath = "";
+        if(FacesContext.getCurrentInstance().getExternalContext().isUserInRole(RoleName.ADMIN)){
+            rolePath = "admin";
+        }
+        if(FacesContext.getCurrentInstance().getExternalContext().isUserInRole(RoleName.TEACHER)){
+            rolePath = "teacher";
+        }
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("http://localhost:1111/TestFiller-web-1.0-SNAPSHOT/faces/" + rolePath +"/index.xhtml");
+        } catch (IOException ex) {
+            LOGGER.info(ex.getMessage());
+        }
+    }
+    
+    public String logout(){
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "home";
     }
 }
