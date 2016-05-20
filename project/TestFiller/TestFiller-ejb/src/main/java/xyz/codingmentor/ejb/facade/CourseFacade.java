18903436736d22553df9e433907020d280a75c6f
Balázs Borján <balazs.borjan@gmail.com>
2016-05-20 15:01:33 +0200

    in progress

--- project/TestFiller/TestFiller-ejb/src/main/java/xyz/codingmentor/ejb/facade/CourseFacade.java
+++ project/TestFiller/TestFiller-ejb/src/main/java/xyz/codingmentor/ejb/facade/CourseFacade.java
@@ -2,12 +2,21 @@ package xyz.codingmentor.ejb.facade;
 
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import xyz.codingmentor.entity.Course;
 
 @Stateless(name = "courseFacade")
 public class CourseFacade extends EntityFacade{
 
+    
+    
     public List<Course> getActiveCourses(){
         return entityManager.createNamedQuery("COURSES.findActiveCourses", Course.class).getResultList();
     }
+    
+//    public List<Course> getSubscribedCourses(){
+//        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+//        
+//    }
 }
