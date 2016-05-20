00bb5b3f73cb7093fda6b748004e7f0adc59ff81
Bence Németh <nembence1994@gmail.com>
2016-05-20 20:53:00 +0200

    wannabe eager

--- project/TestFiller/TestFiller-web/src/main/java/xyz/codingmentor/ejb/TeacherController.java
+++ project/TestFiller/TestFiller-web/src/main/java/xyz/codingmentor/ejb/TeacherController.java
@@ -1,5 +1,6 @@
 package xyz.codingmentor.ejb;
 
+import java.util.Arrays;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -7,6 +8,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import xyz.codingmentor.ejb.facade.EntityFacade;
 import xyz.codingmentor.entity.Course;
+import xyz.codingmentor.entity.Question;
 import xyz.codingmentor.entity.Teacher;
 import xyz.codingmentor.entity.Test;
 
@@ -18,10 +20,49 @@ public class TeacherController {
     
     private Teacher loggedInTeacher;
     
+    
+    private List<String> questionTypes = Arrays.asList("Text type", "Optional type");
+    
+    private Test test;
+
+    private Question question;
+    
     @PostConstruct
     public void init(){
         loggedInTeacher = ef.namedQueryOneParam("TEACHER.findByEmail", Teacher.class, "email",
                 FacesContext.getCurrentInstance().getExternalContext().getRemoteUser()).get(0);
+        test = new Test();
+        question = new Question();
+    }
+
+    public void create(){
+        loggedInTeacher.getTests().add(test);
+        test.setTeacher(loggedInTeacher);
+        ef.update(loggedInTeacher);
+    }
+    
+    public List<Question> getQuestions(){
+        return test.getQuestions();
+    }
+    
+    public List<String> getQuestionTypes() {
+        return questionTypes;
+    }
+
+    public Test getTest() {
+        return test;
+    }
+
+    public void setTest(Test test) {
+        this.test = test;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
     }
     
     public List<Test> getTests(){
