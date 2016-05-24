38d24f7324ef400ff1aaf88cb397806982ea6b5b
Rotttenhoffer Olivér <dvdrip.xvid.avi@gmail.com>
2016-05-24 23:17:12 +0200

    add configuration

---
+++ wildfly_config.sh
@@ -0,0 +1,2 @@
+cp -f configuration/standalone-full.xml wildfly/wildfly-10.0.0.Final/standalone/configuration/
+cp -r -f configuration/mysql/ wildfly/wildfly-10.0.0.Final/modules/system/layers/base/com/
